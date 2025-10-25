d8bda4289724760d49d4510c2fb7ffc2c25e6ae2
z1u1b <ars1for1work@gmail.com>
2025-10-25 12:36:11 +0300

    Input one uint

--- main.cpp
+++ main.cpp
@@ -1,8 +1,20 @@
 #include <iostream>
 int main() 
 {
-  using namespace std;
-  size_t result = 0;
-  cout << result << endl;
-  return 0;
+//   using namespace std;
+  using u_t=size_t;
+  u_t a=0;
+  std::cin>>a;
+  if (std::cin.eof()) {
+      std::cout << 0;
+      std::cout << "\n";
+  }
+  else if (std::cin.fail())
+  {
+      std::cerr<<"Error\n";
+      return 1;
+  }
+//   size_t result = 0;
+//   cout << result << endl;
+//   return 0;
 }
