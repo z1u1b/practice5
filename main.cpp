fc327d4704803b0b857c47e6f40b7c65dea096f2
z1u1b <ars1for1work@gmail.com>
2025-10-25 18:21:48 +0300

    Complete classroom

--- main.cpp
+++ main.cpp
@@ -1,32 +1,66 @@
 #include <iostream>
-bool isPyth(size_t a, size_t b, size_t c)
+#include <limits>
+using u_t = short;
+u_t max_u()
 {
+  using namespace std;
+  using u_limit = numeric_limits<u_t>;
+  return u_limit::max();
+}
+bool isPyth(u_t a, u_t b, u_t c)
+{
+  bool conForOverflowMult = (a > max_u() / a);
+  conForOverflowMult = conForOverflowMult || (b > max_u() / b);
+  conForOverflowMult = conForOverflowMult || (c > max_u() / c);
+  if (conForOverflowMult)
+  {
+    throw std::range_error("Input value out of range");
+  } else {
+    bool conForOverflowPlus = (a * a > max_u() - b * b);
+    conForOverflowPlus = conForOverflowPlus || (a * a > max_u() - c * c);
+    conForOverflowPlus = conForOverflowPlus || (b * b > max_u() - c * c);
+    if (conForOverflowPlus)
+    {
+      throw std::range_error("Input value out of range");
+    }
+  }
+
   bool con = (a * a + b * b == c * c);
   con = con || (a * a + c * c == b * b);
   con = con || (b * b + c * c == a * a);
-  return con;
-  
-}
-int main() 
-{
-//   using namespace std;
-  using u_t=size_t;
-  u_t a=0,b=0,c=0;
-  std::cin>>c>>b;
-  u_t count=0;
 
-  while (std::cin>>a) {
-    count+=isPyth(a,b,c) ? 1 : 0;
-    c=b;
-    b=a;
-  }
-  if (std::cin.eof()) {
+  
+  return con;
+}
+
+int main()
+{
+  u_t a = 0, b = 0, c = 0;
+  try
+  {
+    std::cin >> c >> b;
+    u_t count = 0;
+
+    while (std::cin >> a)
+    {
+      count += isPyth(a, b, c) ? 1 : 0;
+      c = b;
+      b = a;
+    }
+    if (std::cin.eof())
+    {
       std::cout << count;
       std::cout << "\n";
-  }
-  else if (std::cin.fail())
-  {
-      std::cerr<<"Error\n";
+    }
+    else if (std::cin.fail())
+    {
+      std::cerr << "Error\n";
       return 1;
+    }
+  }
+  catch (const std::range_error &e)
+  {
+    std::cerr << "Exception: " << e.what() << "\n";
+    return 2;
   }
 }
